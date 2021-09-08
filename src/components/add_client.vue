75f30f49551a34823c96a7b7f910d6115798790f
Odumz <odumz20@gmail.com>
2021-09-08 02:15:00 +0100

    home view updated; tailwind initialized; main css file added;

--- src/components/add_client.vue
+++ src/components/add_client.vue
@@ -14,7 +14,7 @@
             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div class="sm:flex sm:items-start">
                 <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                  <ExclamationIcon class="h-6 w-6 text-red-600" aria-hidden="true" />
+                  <Icon icon="bi:exclamation-triangle" class="h-6 w-6 text-red-600" aria-hidden="true" />
                 </div>
                 <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                   <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900">
@@ -44,18 +44,18 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { Dialog, DialogOverlay, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
-import { ExclamationIcon } from '@heroicons/vue/outline'
+import { Icon } from '@iconify/vue'
 
-export default {
+export default defineComponent({
   components: {
     Dialog,
     DialogOverlay,
     DialogTitle,
     TransitionChild,
     TransitionRoot,
-    ExclamationIcon
+    Icon
   },
   setup () {
     const open = ref(true)
@@ -64,5 +64,5 @@ export default {
       open
     }
   }
-}
+})
 </script>
