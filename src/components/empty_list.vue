0d911b5068d39e4042fddb0443a38449e988baf2
Odumz <odumz20@gmail.com>
2021-10-08 23:41:29 +0100

    seo improvements

--- src/components/empty_list.vue
+++ src/components/empty_list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <div class="flex justify-center content-center p-4" :class="hide">
-      <img src="https://res.cloudinary.com/griffintech/image/upload/v1631703277/tutorials/3009287_bfc6nl.jpg" class="md:w-2/7">
+      <img src="https://res.cloudinary.com/griffintech/image/upload/v1631703277/tutorials/3009287_bfc6nl.jpg" alt="empty" class="md:w-2/7">
     </div>
     <p class="flex items-center justify-center"><slot></slot></p>
     <button @click.prevent="eventHandler" class="text-sm px-4 py-3 my-3 rounded border shadow-sm border-gray-300" :class="hide">Add A New Client</button>
